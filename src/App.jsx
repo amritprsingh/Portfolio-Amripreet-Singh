d986c39c394d4a50d64c15ff76406e00d1e2b0bb
Amritpreet Singh <amritpreetsingh0915@gmail.com>
2024-11-24 23:26:35 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Experience } from "./components/Experience/Experience";
 import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Projects } from "./components/Projects/Projects";
+import {TechProjects} from "./components/Projects/TechProjects";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Hero />
       <Experience />
       <Projects />
+      <TechProjects />
       <About />
       <Contact />
     </div>
